fadbb2473f281db619d1db31200fc9203d82c050
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2026-01-14 22:58:18 -0500

    do it

---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+	import bg from '$lib/assets/bg.jpg';
+	import hackpad from '$lib/assets/hackpad.png';
+	import football from '$lib/assets/football.png';
+	import bingo from '$lib/assets/binary.png';
+	import Card from './components/card.svelte';
+</script>
+
+<div
+	style="background-image: url({bg})"
+	class="flex min-h-screen flex-col items-center justify-center gap-8 bg-cover bg-center"
+>
+	<div
+		class="w-1/2 rounded-2xl bg-cyan-950/40 p-4 text-center backdrop-blur-xl transition-all hover:scale-110 md:p-16"
+	>
+		<h1 class="text-2xl text-cyan-50 md:text-5xl">Hi there! 👋</h1>
+		<h2 class="text-lg text-cyan-50 md:text-2xl">
+			I'm Noah Siegel, a High Schooler passionate about all things tech.
+		</h2>
+	</div>
+	<div
+		class="w-3/4 rounded-2xl bg-cyan-950/40 p-16 text-xl text-cyan-50 backdrop-blur-xl md:text-3xl"
+	>
+		<h1 class="mb-2 text-3xl">Here's some things I've made...</h1>
+		<div class="flex flex-col gap-4 md:flex-row">
+			<Card
+				img={hackpad}
+				title="QLab Macropad"
+				text="A macropad I designed and built that can do different macros for QLab, a sound and lighting control software."
+				link="https://github.com/TheSeagullDev/theseagulldevs-ultimate-hackpad"
+			></Card>
+			<Card
+				img={football}
+				title="College Football Picker"
+				text="A full-stack app made in with flask and SQLite to run a family college football tournament. Excel spreadsheets no more!"
+				link="https://github.com/TheSeagullDev/college-football-2"
+			></Card>
+			<Card
+				img={bingo}
+				title="Binary Bingo Dashboard"
+				text="A simple dashboard made in Svelte to run a &quot;Binary Bingo&quot; event to teach middle schoolers the fundamentals of computer science!"
+				link="https://github.com/TheSeagullDev/binary-bingo"
+			></Card>
+		</div>
+	</div>
+</div>
---
+++ src/routes/components/card.svelte
@@ -0,0 +1,11 @@
+<script>
+    let { img, title, text, link } = $props();
+</script>
+
+<a class="flex flex-col items-center w-2/3 bg-cyan-200/10 rounded-2xl shadow-md backdrop-blur-2xl p-4 gap-2" href={link}>
+	<h2 class="text-2xl">{title}</h2>
+	<img src={img} width="256px" alt="" class="rounded-2xl object-fill" />
+	<p class="text-lg">
+		{text}
+	</p>
+</a>
