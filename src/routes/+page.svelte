c54feb5a774f87a7399ab6b028fc162e88243f86
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2026-01-17 09:51:43 -0500

    make it more responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,21 +8,21 @@
 
 <div
 	style="background-image: url({bg})"
-	class="flex min-h-screen flex-col items-center justify-center gap-8 bg-cover bg-center"
+	class="flex min-h-screen flex-col items-center justify-center gap-8 bg-cover bg-center p-4"
 >
 	<div
-		class="w-1/2 rounded-2xl bg-cyan-950/40 p-4 text-center backdrop-blur-xl transition-all hover:scale-110 md:p-16"
+		class="w-1/2 rounded-2xl bg-cyan-950/40 p-4 text-center backdrop-blur-xl transition-all hover:scale-110 xl:p-16"
 	>
-		<h1 class="text-2xl text-cyan-50 md:text-5xl">Hi there! 👋</h1>
-		<h2 class="text-lg text-cyan-50 md:text-2xl">
+		<h1 class="text-2xl text-cyan-50 xl:text-5xl">Hi there! 👋</h1>
+		<h2 class="text-xl text-cyan-50 xl:text-2xl">
 			I'm Noah Siegel, a High Schooler passionate about all things tech.
 		</h2>
 	</div>
 	<div
-		class="w-3/4 rounded-2xl bg-cyan-950/40 p-16 text-xl text-cyan-50 backdrop-blur-xl md:text-3xl"
+		class="md:w-3/4 rounded-2xl bg-cyan-950/40 p-4 xl:p-16 text-xl text-cyan-50 backdrop-blur-xl xl:text-3xl"
 	>
-		<h1 class="mb-2 text-3xl">Here's some things I've made...</h1>
-		<div class="flex flex-col gap-4 md:flex-row">
+		<h1 class="mb-2 text-xl xl:text-3xl">Here's some things I've made...</h1>
+		<div class="flex flex-col gap-4 md:flex-row items-center">
 			<Card
 				img={hackpad}
 				title="QLab Macropad"
--- src/routes/components/card.svelte
+++ src/routes/components/card.svelte
@@ -2,9 +2,9 @@
     let { img, title, text, link } = $props();
 </script>
 
-<a class="flex flex-col items-center w-2/3 bg-cyan-200/10 rounded-2xl shadow-md backdrop-blur-2xl p-4 gap-2" href={link}>
-	<h2 class="text-2xl">{title}</h2>
-	<img src={img} width="256px" alt="" class="rounded-2xl object-fill" />
+<a class="flex flex-col items-center md:w-2/3 bg-cyan-200/10 rounded-2xl shadow-md backdrop-blur-2xl p-4 gap-2" href={link}>
+	<h2 class="text-md xl:text-2xl">{title}</h2>
+	<img src={img} alt="" class="w-32 xl:w-64 rounded-2xl object-fill" />
 	<p class="text-lg">
 		{text}
 	</p>
